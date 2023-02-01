1588c861bd85ab22bece49e2d88be01c63b2bd2c
“EvilisGlenio” <glenio.developer@gmail.com>
2023-02-01 09:42:17 -0300

    atualização do projeto mentalista

--- 2ºDia-Mentalista/script.js
+++ 2ºDia-Mentalista/script.js
@@ -1,15 +1,19 @@
 var numSecreto = Math.floor(Math.random() * 1000)  + 1;
 
 var chute = prompt('Digite um número entre 1 e 1000')
+var cont = 1
 
 while (chute != numSecreto) {
     if(chute > numSecreto) {
-        alert('O numero é menor, try again!')
+        alert('Tentativa de número '+cont)
+        alert('O numero é menor que '+chute+', try again!')
         chute = prompt('Digite novamente um número entre 1 e 1000')
     } else {
-        alert('O número é maior, try again!!')
+        alert('Tentativa de número '+cont)
+        alert('O número é maior que '+chute+', try again!!')
         chute = prompt('Digite novamente um número entre 1 e 1000')
     }
+    cont++
 } 
     alert("Parabéns, você acertou!!")
 
