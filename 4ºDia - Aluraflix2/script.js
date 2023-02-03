fb607b82cf7cbd1f886c17411caf751bebdb66ba
“EvilisGlenio” <glenio.developer@gmail.com>
2023-02-03 11:30:28 -0300

    Atualização do Aluraflix2

--- 4ºDia - Aluraflix2/script.js
+++ 4ºDia - Aluraflix2/script.js
@@ -1,12 +1,46 @@
-function adicionarFilme() {
-    var filmeFavorito = document.querySelector('#filme').value
-    var elementMovieList = document.getElementById('listaFilmes')
+const res = document.querySelector('#res')
+var elementMovieList = document.getElementById('listaFilmes')
+var filmeFavorito = []
+var i = 0
 
-    elementMovieList.innerHTML += '<img src='+filmeFavorito+'>'
+function adicionarFilme() {
+    //Validador de link
+    if(document.querySelector('#filme').value.indexOf('.jpg') != -1) {
+        filmeFavorito[i] = document.querySelector('#filme').value
+        try {
+            extrairTexto(filmeFavorito[i])
+        } catch (error) {
+            console.log('erro', error)
+        }
+
+    } else {
+        return alert('Link inválido! Try again!')
+    }
+
+    elementMovieList.innerHTML += '<a href="'+'./index.html"'+'target="_blank"'+'>'+'<img src='+filmeFavorito[i]+'>'+'</a>'
     document.getElementById('filme').value = ''
+    i++
 }
 
+//Extrator de Texto apartir de uma Imagem
+async function extrairTexto(img) {
 
+    const worker = await Tesseract.createWorker({
+        logger: m => console.log(m)
+    });
+    
+    (async () => {
+        await worker.loadLanguage('eng');
+        await worker.initialize('eng');
+        const { data: { text } } = await worker.recognize(img);
+        res.innerHTML = '<p>'+text+'</p>';
+        console.log(text);
+        await worker.terminate();
+        return text;
+    })();
+}
+
+//Filtro de string para fazer pesquisa
 
 //DESAFIOS
 
