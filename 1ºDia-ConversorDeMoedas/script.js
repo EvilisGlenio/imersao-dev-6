99832d65fb03f8da6eae6baeaa324c27a4786b67
“EvilisGlenio” <glenio.developer@gmail.com>
2023-02-04 12:33:25 -0300

    att Conversor Interestrelar

--- 1ºDia-ConversorDeMoedas/script.js
+++ 1ºDia-ConversorDeMoedas/script.js
@@ -1,18 +1,12 @@
-/* var valorEmDolar = 60
- var cotacaoDoDolar = 5.32
- var valorEmReal = valorEmDolar*cotacaoDoDolar
+ //Entrada de dados 
+ const usuario = prompt("Qual o seu nome?")
+ const star = prompt("Qual o nome da estrela?")
+ var lightYears = prompt("A quantos anos luz ela está da terra")
 
- alert('$ '+valorEmDolar+' dolares está cotado em R$ '+(valorEmReal.toFixed(2))+ ' reais.')
- */
+//Processamento dos Dados
+let distance = lightYears
+let convertorLY = distance * 9460536068.016
 
- const usuario = document.querySelector('.name')
- const star = document.querySelector('.star')
- var lightYears = document.querySelector('.distance')//4.35
- const res = document.getElementById('res')
-
- function calcular() {
-    let distance = lightYears.value
-   let convertorLY = distance * 9460536068.016
-   res.innerText = "A distância entre "+star.value+" e a terra é de "+lightYears.value+" anos luz, que é equivalente à "+convertorLY.toFixed(1)+" km."
- }
+//Saída de Dados
+alert(usuario+", a distancia entre "+star+" e a terra é de "+distance+" ou seja,"+convertorLY+ " km.")
 
